663e334be0f7233eaeb076f31c8b32c7217501f0
Egzona Vllasaliu <egzona.vllasaliu@studentet.uni-pr.edu>
2022-12-04 12:39:53 +0100

    Add structure of elgamal, key struct

---
+++ ElgamalExtension/ElgamalExtension/ElGamal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Xml;
+
+namespace ElgamalEncryption.Algorithm
+{
+    public abstract class ElGamal : AsymmetricAlgorithm
+    {
+        public abstract void ImportParameters(ElGamalKeyStruct keyStruct);
+        public abstract byte[] Sign(byte[] p_hashcode);
+    }
+}
---
+++ ElgamalExtension/ElgamalExtension/ElGamalKeyStruct.cs
@@ -0,0 +1,15 @@
+﻿using ElgamalEncryption.Algorithm.misc;
+
+namespace ElgamalEncryption.Algorithm
+{
+    public struct ElGamalKeyStruct
+    {
+        //Those will be contained inside the public key
+        public BigInteger P;
+        public BigInteger G;
+        public BigInteger Y;
+
+        //This will be the private key
+        public BigInteger X;
+    }
+}
